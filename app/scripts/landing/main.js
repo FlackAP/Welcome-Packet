3728c914d7aa6ed1b657cc4b665c8229fc23fb6a
FlackAP <flack.ap@gmail.com>
2013-11-19 15:45:45 -0500

    added dropdown box with styles

---
+++ app/scripts/landing/main.js
@@ -0,0 +1,26 @@
+$('#cssmenu ul ul li:odd').addClass('odd');
+$('#cssmenu ul ul li:even').addClass('even');
+
+$('#cssmenu > ul > li > a').click(function() {
+
+  var checkElement = $(this).next();
+
+  $('#cssmenu li').removeClass('active');
+  $(this).closest('li').addClass('active'); 
+
+  if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    $(this).closest('li').removeClass('active');
+    checkElement.slideUp('normal');
+  }
+  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    $('#cssmenu ul ul:visible').slideUp('normal');
+    checkElement.slideDown('normal');
+  }
+
+  if($(this).closest('li').find('ul').children().length == 0) {
+    return true;
+  } else {
+    return false; 
+  }
+
+});
